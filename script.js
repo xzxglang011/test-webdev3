5ce011fe99d0a9a854e3169a0405bfa094a7330d
Krittaphat Oncharoen <54741866+xzxglang011@users.noreply.github.com>
2024-02-22 23:01:06 +0700

    Add files via upload

---
+++ script.js
@@ -0,0 +1,56 @@
+const menuIcon = document.querySelector(".menu-icon")
+const xBtn = document.querySelector(".x-btn")
+const navigation = document.querySelector(".navigation")
+let start = false
+
+menuIcon.addEventListener("click", () => {
+  navigation.classList.add("navigate")
+})
+xBtn.addEventListener("click", () => {
+  navigation.classList.remove("navigate")
+})
+
+window.addEventListener("scroll", () => {
+  const about = document.querySelector(".about")
+  const services = document.querySelector(".services")
+  const portfolio = document.querySelector(".portfolio")
+  const data = document.querySelector(".data")
+  const nums = document.querySelectorAll(".num")
+
+  if (window.scrollY >= 200) {
+    about.classList.add("change")
+  } else {
+    about.classList.remove("change")
+  }
+
+  if (window.scrollY >= about.offsetTop + 200) {
+    services.classList.add("change")
+  } else {
+    services.classList.remove("change")
+  }
+
+  if (window.scrollY >= services.offsetTop) {
+    portfolio.classList.add("change")
+  } else {
+    portfolio.classList.remove("change")
+  }
+
+  if (window.scrollY >= data.offsetTop - 300) {
+    if (!start) {
+      nums.forEach((num) => {
+        startCount(num)
+      })
+    }
+    start = true
+  }
+})
+
+const startCount = (el) => {
+  let max = el.dataset.val;
+  let count = setInterval(() => {
+    el.textContent++
+    if (el.textContent === max) {
+      clearInterval(count)
+    }
+  }, 5);
+}
